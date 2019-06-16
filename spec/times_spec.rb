9f17daa617da7c283339ba2631fbb14b3520febe
Rashard Pleasant <rashard.pleasant@gmail.com>
2019-06-16 01:40:16 +0000

    Automatically backed up by Learn

--- spec/times_spec.rb
+++ spec/times_spec.rb
@@ -5,7 +5,8 @@ describe "#using_times" do
 	it "puts out the phrase 'Wingardium Leviosa' 7 times by using the `times` construct" do
 
  7.times do
-	puts "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
+	puts "Wingardium Leviosa."
+	"Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
 	expect{ using_times }.to output(looping_string).to_stdout 
 	end
 end
